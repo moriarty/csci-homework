73b8e30e452a0bb55bd0839caec91ab0a0dbd4f1
Alexander Moriarty <alexander@dal.ca>
2012-09-13 12:16:28 -0300

    continued q3

--- 4155/assignments/A1/code/a1.3.py
+++ 4155/assignments/A1/code/a1.3.py
@@ -3,25 +3,21 @@ from pylab import *
 from mpl_toolkits.mplot3d import Axes3D
 
 SIGMA = array([[1, 0.5],[0.5,1]])
-MU = array([[1],[2]])
-
 
 X,Y = mgrid[-2:4:.1, -2:4:.1]
 
-x = array([[X],[Y]])
+
 
 part1 = ((linalg.det(2*pi*SIGMA))**(-0.5))
 
-part2 = (-0.5)*(x-MU).conj().transpose()*linalg.inv(SIGMA)*(x-MU)
+part2 = dot((x-MU), dot(sigma,(x-MU))
 
- part2
+print part2
 
 part3 = exp(-0.5*part2)
-part3
 
 Z=part1*part3
 
 ax = gca(projection='3d')
 ax.plot_surface(X,Y,Z, rstride=1,cstride=1,cmap=cm.jet, linewidth=0,antialiased=False)
 show()
-
