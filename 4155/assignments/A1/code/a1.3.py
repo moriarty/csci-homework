ce2bc2b3fc66e9dbc50de8549776e7a308350677
Alexander Moriarty <alexander@dal.ca>
2012-09-13 15:27:58 -0300

    q3

--- 4155/assignments/A1/code/a1.3.py
+++ 4155/assignments/A1/code/a1.3.py
@@ -1,22 +1,15 @@
 from matplotlib.pyplot import *
 from pylab import *
 from mpl_toolkits.mplot3d import Axes3D
-
-SIGMA = array([[1, 0.5],[0.5,1]])
-
+SIGMA = matrix([[1, 0.5],[0.5,1]])
+MU = matrix([2, 1])
 X,Y = mgrid[-2:4:.1, -2:4:.1]
 
+part1 = ((2*pi)**(0.5))**(X.shape[0])
+part2 = det(SIGMA)**(-0.5)
+part3 = matrix([X[:,0]-ones(X[:,0].shape)*MU[0,0], Y[0,:]-ones(Y[:,0].shape)*MU[0,1]])
 
-
-part1 = ((linalg.det(2*pi*SIGMA))**(-0.5))
-
-part2 = dot((x-MU), dot(sigma,(x-MU))
-
-print part2
-
-part3 = exp(-0.5*part2)
-
-Z=part1*part3
+Z = part1*part2* exp(-0.5* part3.T * inv(SIGMA) * part3 )
 
 ax = gca(projection='3d')
 ax.plot_surface(X,Y,Z, rstride=1,cstride=1,cmap=cm.jet, linewidth=0,antialiased=False)
